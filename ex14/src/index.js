e44a9f2653f7a67d4a2e14bed25fb325edb0cc19
EdinCehajic <ed.cehajic@gmail.com>
2021-03-12 15:46:44 +0100

    Adding Day05

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    wordBlanks = "My " + myAdjective + " " + myNoun + " " + myVerb + " from you " + myAdverb + ".";
+
+    // Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
